eb4952703eb2e9d576ab7de2e0d470fcfe5fa291
Empika1 <dravidts1@gmail.com>
2025-02-28 18:32:08 -0500

    work with newest ezdrawing version

--- v1.py
+++ v1.py
@@ -1,13 +1,13 @@
-import ezdrawingtk
+import ezdrawingtk as ezdrawing
 
-ezdrawingtk.open_window((800, 800))
+ezdrawing.open_window((800, 800))
 
 last_mouse_pos = (0, 0)
-while not ezdrawingtk.should_quit():
-    mouse_pos = ezdrawingtk.get_mouse_pos()
-    if 1 in ezdrawingtk.get_pressed_mouse_buttons():
-        ezdrawingtk.draw_capped_line((0, 0, 0), last_mouse_pos, mouse_pos, 10)
+while not ezdrawing.should_quit():
+    mouse_pos = ezdrawing.get_mouse_pos()
+    if 1 in ezdrawing.get_pressed_mouse_buttons():
+        ezdrawing.draw_capped_line((0, 0, 0), last_mouse_pos, mouse_pos, 10)
     
     last_mouse_pos = mouse_pos
     
-    ezdrawingtk.update()
+    ezdrawing.update()
--- v3.py
+++ v3.py
@@ -1,9 +1,9 @@
-import ezdrawingtk
+import ezdrawingtk as ezdrawing
 
 color = (0, 0, 0)
 def select_color():
     global color
-    pressed_keys = ezdrawingtk.get_pressed_keys()
+    pressed_keys = ezdrawing.get_pressed_keys()
     if "q" in pressed_keys:
         color = (0, 0, 0)
     elif "w" in pressed_keys:
@@ -18,7 +18,7 @@ def select_color():
 size = 1
 def select_size():
     global size
-    pressed_keys = ezdrawingtk.get_pressed_keys()
+    pressed_keys = ezdrawing.get_pressed_keys()
     if "a" in pressed_keys:
         size = 1
     if "s" in pressed_keys:
@@ -39,7 +39,7 @@ def select_size():
 tool = "pen"
 def select_tool():
     global tool
-    pressed_keys = ezdrawingtk.get_pressed_keys()
+    pressed_keys = ezdrawing.get_pressed_keys()
     if "1" in pressed_keys:
         tool = "pen"
     if "2" in pressed_keys:
@@ -52,60 +52,60 @@ def select_tool():
 last_mouse_pos = (0, 0)
 def use_pen():
     global size, color, last_mouse_pos
-    mouse_pos = ezdrawingtk.get_mouse_pos()
-    if 1 in ezdrawingtk.get_pressed_mouse_buttons():
-        ezdrawingtk.draw_capped_line(color, last_mouse_pos, mouse_pos, size)
+    mouse_pos = ezdrawing.get_mouse_pos()
+    if 1 in ezdrawing.get_pressed_mouse_buttons():
+        ezdrawing.draw_capped_line(color, last_mouse_pos, mouse_pos, size)
     
     last_mouse_pos = mouse_pos
 
 line_start_pos = None
 def use_line():
     global size, color, line_start_pos
-    mouse_pos = ezdrawingtk.get_mouse_pos()
-    if 1 in ezdrawingtk.get_pressed_mouse_buttons():
+    mouse_pos = ezdrawing.get_mouse_pos()
+    if 1 in ezdrawing.get_pressed_mouse_buttons():
         if line_start_pos == None:
             line_start_pos = mouse_pos
         else:
-            ezdrawingtk.undo()
-        ezdrawingtk.draw_line(color, line_start_pos, mouse_pos, size)
+            ezdrawing.undo()
+        ezdrawing.draw_line(color, line_start_pos, mouse_pos, size)
     else:
         line_start_pos = None
 
 rect_start_pos = None
 def use_rect():
     global size, color, rect_start_pos
-    mouse_pos = ezdrawingtk.get_mouse_pos()
-    if 1 in ezdrawingtk.get_pressed_mouse_buttons():
+    mouse_pos = ezdrawing.get_mouse_pos()
+    if 1 in ezdrawing.get_pressed_mouse_buttons():
         if rect_start_pos == None:
             rect_start_pos = mouse_pos
         else:
-            ezdrawingtk.undo()
+            ezdrawing.undo()
         top_left = (min(rect_start_pos[0], mouse_pos[0]), min(rect_start_pos[1], mouse_pos[1]))
         bottom_right = (max(rect_start_pos[0], mouse_pos[0]), max(rect_start_pos[1], mouse_pos[1]))
-        ezdrawingtk.draw_rect(color, top_left, bottom_right)
+        ezdrawing.draw_rect(color, top_left, bottom_right)
     else:
         rect_start_pos = None
 
 ellipse_start_pos = None
 def use_ellipse():
     global size, color, ellipse_start_pos
-    mouse_pos = ezdrawingtk.get_mouse_pos()
-    if 1 in ezdrawingtk.get_pressed_mouse_buttons():
+    mouse_pos = ezdrawing.get_mouse_pos()
+    if 1 in ezdrawing.get_pressed_mouse_buttons():
         if ellipse_start_pos == None:
             ellipse_start_pos = mouse_pos
         else:
-            ezdrawingtk.undo()
+            ezdrawing.undo()
         top_left = (min(ellipse_start_pos[0], mouse_pos[0]), min(ellipse_start_pos[1], mouse_pos[1]))
         bottom_right = (max(ellipse_start_pos[0], mouse_pos[0]), max(ellipse_start_pos[1], mouse_pos[1]))
-        ezdrawingtk.draw_ellipse(color, top_left, bottom_right)
+        ezdrawing.draw_ellipse(color, top_left, bottom_right)
     else:
         ellipse_start_pos = None
 
-ezdrawingtk.open_window((800, 800))
+ezdrawing.open_window((800, 800), "Paint")
 
 last_mouse_pos = (0, 0)
-while not ezdrawingtk.should_quit():
-    if not 1 in ezdrawingtk.get_pressed_mouse_buttons():
+while not ezdrawing.should_quit():
+    if not 1 in ezdrawing.get_pressed_mouse_buttons():
         select_color()
         select_size()
         select_tool()
@@ -119,8 +119,7 @@ while not ezdrawingtk.should_quit():
     else:
         use_ellipse()
     
-    if "x" in ezdrawingtk.get_pressed_keys():
-        ezdrawingtk.save("x.png")
+    if "x" in ezdrawing.get_pressed_keys():
+        ezdrawing.save("x.png")
     
-    ezdrawingtk.update()
-ezdrawingtk.quit()
+    ezdrawing.update()
